81f08d20496d67c7ec88650a6cf8e65a044358d1
kingstar718 <936016045@qq.com>
2019-04-07 23:45:18 +0800

    leetcode

---
+++ src/main/java/top/wujinxing/LeetCode/DynamicProgramming/_213_House_Robber.java
@@ -0,0 +1,29 @@
+package top.wujinxing.LeetCode.DynamicProgramming;
+
+/**
+ * @author: wujinxing
+ * @date: 2019/4/7 23:44
+ * @description: 强盗在环形街区抢劫
+ */
+public class _213_House_Robber {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        return Math.max(rob(nums, 0, n - 2), rob(nums, 1, n - 1));
+    }
+
+    private int rob(int[] nums, int first, int last) {
+        int pre2 = 0, pre1 = 0;
+        for (int i = first; i <= last; i++) {
+            int cur = Math.max(pre1, pre2 + nums[i]);
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1;
+    }
+}
