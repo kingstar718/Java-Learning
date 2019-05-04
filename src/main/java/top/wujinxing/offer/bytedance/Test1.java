6dd505f1c9250326e62912ff73640099a6eb6c1f
kingstar718 <kingstar718@foxmail.com>
2019-05-05 00:02:06 +0800

    offer code

--- src/main/java/top/wujinxing/offer/bytedance/Test1.java
+++ src/main/java/top/wujinxing/offer/bytedance/Test1.java
@@ -33,6 +33,21 @@ public class Test1 {
         System.out.println(Integer.max(change(n, m, s, 'a'), change(n, m , s ,'b')));
     }
 
+    /**
+     * 利用前缀和数组
+     * 例如：
+     * n=10,m=1,s=baabaabaab
+     *   b a a b a a b a a b
+     *   0 1 2 3 4 5 6 7 8 9
+     *
+     *   将 b-->a
+     *   b 的前缀和数组为
+     *          sums={ 0, 3, 6, 9, 10}//10 为字符串长度
+     *   计算长度分别为:
+     *          3  6-0-1=5   9-3-1=5  10-6-1=3
+     *     ==>>max = max{ max, sums[i]-sum[i-m-1]-1}
+     */
+
     private static int change(int n, int m, String s, char k){
         int max = 0;
         List<Integer> sums = new ArrayList<>();
