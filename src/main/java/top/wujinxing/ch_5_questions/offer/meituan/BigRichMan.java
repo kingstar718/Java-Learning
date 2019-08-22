7f065d32cc7e4de50f67cedb85c50f6a169a68c1
kingstar718 <936016045@qq.com>
2019-08-22 20:57:42 +0800

    美团

--- src/main/java/top/wujinxing/ch_5_questions/offer/meituan/BigRichMan.java
+++ src/main/java/top/wujinxing/ch_5_questions/offer/meituan/BigRichMan.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 /**
  * @author wujinxing
  * date 2019 2019/8/7 18:52
- * description
+ * description  未完成
+ *
  * 大富翁游戏，玩家根据骰子的点数决定走的步数，即骰子点数为1时可以走一步，
  * 点数为2时可以走两步，点数为n时可以走n步。求玩家走到第n步（n<=骰子最大点数且是方法的唯一入参）时，
  * 总共有多少种投骰子的方法。
