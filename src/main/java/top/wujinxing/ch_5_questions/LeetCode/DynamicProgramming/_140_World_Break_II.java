b740cad2bced583ea0d6014d14990bd4c4bb7485
kingstar718 <936016045@qq.com>
2019-08-22 20:57:31 +0800

    DP

---
+++ src/main/java/top/wujinxing/ch_5_questions/LeetCode/DynamicProgramming/_140_World_Break_II.java
@@ -0,0 +1,56 @@
+package top.wujinxing.ch_5_questions.LeetCode.DynamicProgramming;
+
+import java.util.List;
+
+/**
+ * @author wujinxing
+ * date 2019 2019/8/22 14:34
+ * description
+ *
+ * Given a non-empty string s and a dictionary wordDict containing a list of non-empty words,
+ * add spaces in s to construct a sentence where each word is a valid dictionary word.
+ * Return all such possible sentences.
+ *给定非空字符串s和包含非空单词列表的字典wordDict，在s中添加空格以构造句子，其中每个单词是有效的字典单词。
+ * 返回所有这些可能的句子。
+ *
+ * Note:
+ * The same word in the dictionary may be reused multiple times in the segmentation.
+ * You may assume the dictionary does not contain duplicate words.
+ *字典中的相同单词可以在分段中多次重复使用。
+ * 您可能认为字典不包含重复的单词。
+ *
+ * Example 1:
+ * Input:
+ * s = "catsanddog"
+ * wordDict = ["cat", "cats", "and", "sand", "dog"]
+ * Output:
+ * [
+ *   "cats and dog",
+ *   "cat sand dog"
+ * ]
+ *
+ * Example 2:
+ * Input:
+ * s = "pineapplepenapple"
+ * wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+ * Output:
+ * [
+ *   "pine apple pen apple",
+ *   "pineapple pen apple",
+ *   "pine applepen apple"
+ * ]
+ * Explanation: Note that you are allowed to reuse a dictionary word.
+ *
+ * Example 3:
+ * Input:
+ * s = "catsandog"
+ * wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * Output:
+ * []
+ */
+public class _140_World_Break_II {
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        return null;
+    }
+}
