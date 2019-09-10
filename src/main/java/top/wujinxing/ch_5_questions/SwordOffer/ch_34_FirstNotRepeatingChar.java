75690fbcc12428da8ce34c7fb889160173950b78
kingstar718 <936016045@qq.com>
2019-09-11 00:18:26 +0800

    change

--- src/main/java/top/wujinxing/ch_5_questions/SwordOffer/ch_34_FirstNotRepeatingChar.java
+++ src/main/java/top/wujinxing/ch_5_questions/SwordOffer/ch_34_FirstNotRepeatingChar.java
@@ -1,12 +1,43 @@
 package top.wujinxing.ch_5_questions.SwordOffer;
 
+import java.util.HashMap;
+
 /**
  * @author wujinxing
  * date 2019 2019/9/10 23:52
  * description 第一个只出现一次的字符
- *
+ * <p>
  * 在一个字符串(0<=字符串长度<=10000，全部由字母组成)中找到第一个只出现一次的字符,
  * 并返回它的位置, 如果没有则返回 -1（需要区分大小写）.
  */
 public class ch_34_FirstNotRepeatingChar {
+
+    public int FirstNotRepeatingChar(String str) {
+        if (str == null || str.length() == 0)
+            return -1;
+        //因为字母`ascii`在`65 ~ 122`，所以开一个`58`的数组就可以了。
+        int[] counts = new int[58];
+        for (int i = 0; i < str.length(); i++) {
+            counts[str.charAt(i) - 'A']++;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (counts[str.charAt(i) - 'A'] == 1)
+                return i;
+        }
+        return -1;
+    }
+
+    public int FirstNotRepeatingChar2(String str) {
+        if (str == null || str.length() == 0)
+            return -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            map.put(str.charAt(i), map.getOrDefault(str.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (map.get(str.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
 }
---
+++ src/main/java/top/wujinxing/ch_5_questions/SwordOffer/ch_35_InversePairs.java
@@ -0,0 +1,29 @@
+package top.wujinxing.ch_5_questions.SwordOffer;
+
+/**
+ * @author wujinxing
+ * date 2019 2019/9/11 0:16
+ * description 数组中的逆序对
+ * <p>
+ * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+ * 输入一个数组,求出这个数组中的逆序对的总数P。并将P对1000000007取模的结果输出。 即输出P%1000000007
+ * 输入描述:
+ * 题目保证输入的数组中没有的相同的数字
+ * <p>
+ * 数据范围：
+ * 对于%50的数据,size<=10^4
+ * 对于%75的数据,size<=10^5
+ * 对于%100的数据,size<=2*10^5
+ * <p>
+ * 示例1
+ * 输入
+ * 1,2,3,4,5,6,7,0
+ * 输出
+ * 7
+ */
+public class ch_35_InversePairs {
+
+    public int InversePairs(int[] array) {
+        return 0;
+    }
+}
