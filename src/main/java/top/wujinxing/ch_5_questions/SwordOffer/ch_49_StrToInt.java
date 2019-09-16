e2ab72c5436fcbf9a988d0aa83fa221ba3b6819a
kingstar718 <93606045@qq.com>
2019-09-16 17:19:59 +0800

    change

--- src/main/java/top/wujinxing/ch_5_questions/SwordOffer/ch_49_StrToInt.java
+++ src/main/java/top/wujinxing/ch_5_questions/SwordOffer/ch_49_StrToInt.java
@@ -4,22 +4,22 @@ package top.wujinxing.ch_5_questions.SwordOffer;
 import org.junit.Test;
 
 /**
- *  Str to Int
+ * Str to Int
  */
 public class ch_49_StrToInt {
 
-    public int StrToInt(String str){
-        if (str==null||str.trim().equals(" "))
+    public int StrToInt(String str) {
+        if (str == null || str.trim().equals(" "))
             return 0;
 
         char[] chars = str.trim().toCharArray();
         int res = 0;
-        for (int i=(chars[0]=='-'||chars[0]=='+')?1:0; i<str.length(); i++){
-            if (chars[i]<'0'||chars[i]>'9')
+        for (int i = (chars[0] == '-' || chars[0] == '+') ? 1 : 0; i < str.length(); i++) {
+            if (chars[i] < '0' || chars[i] > '9')
                 return 0;
             int num = chars[i] - '0';
-            int sum = res*10+num;
-            if ((sum-num)/10!=res)
+            int sum = res * 10 + num;
+            if ((sum - num) / 10 != res)
                 return 0;
             res = sum;
         }
@@ -27,7 +27,7 @@ public class ch_49_StrToInt {
     }
 
     @Test
-    public void test(){
+    public void test() {
         String s1 = "-283827382";
         String s2 = "22421";
         System.out.println(StrToInt(s1));
