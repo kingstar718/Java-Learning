54459ec7745689a65e4971ae0ad6838691fe51f1
kingstar718 <936016045@qq.com>
2019-10-12 21:46:26 +0800

    LinkedList

---
+++ src/main/java/top/wujinxing/ch_5_questions/LeetCode/LinkedList/ch_160_IntersectionofTwoLinkedLists.java
@@ -0,0 +1,38 @@
+package top.wujinxing.ch_5_questions.LeetCode.LinkedList;
+
+/**
+ * @author wujinxing
+ * date 2019 2019/10/12 21:40
+ * description 两个单链表的交叉节点
+ * <p>
+ * 编写一个程序，找到两个单链表相交的起始节点。
+ * <p>
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Reference of the node with value = 8
+ * 输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ * <p>
+ * 输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * 输出：Reference of the node with value = 2
+ * 输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ * <p>
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 解释：这两个链表不相交，因此返回 null。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
+ */
+public class ch_160_IntersectionofTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        /**
+         *思路：pA指向headA，pB指向headB，一起向前，当某一个走到尽头时，将其赋给另一个链表，直到pA==pB
+         */
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
