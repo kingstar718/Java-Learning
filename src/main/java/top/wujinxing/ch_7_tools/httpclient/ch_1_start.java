09f24e4d3f7e6874a409b0c5d02b1fe8acea4685
kingstar718 <kingstar718@foxmail.com>
2020-10-15 00:11:36 +0800

    csv 读写 httpclient

---
+++ src/main/java/top/wujinxing/ch_7_tools/httpclient/ch_1_start.java
@@ -0,0 +1,121 @@
+package top.wujinxing.ch_7_tools.httpclient;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author wujinxing
+ * date 2019/4/24 16:17
+ * description
+ */
+public class ch_1_start {
+    public static void main(String[] args) {
+        //创建httpClient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        //创建httpGEt
+        String url = "https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=18829038821";
+        HttpGet httpGet = new HttpGet(url);
+        try {
+            CloseableHttpResponse response = httpClient.execute(httpGet);
+            try {
+                HttpEntity httpEntity = response.getEntity();
+                String responseStr = EntityUtils.toString(httpEntity);
+                TelResult telResult = JSON.parseObject(responseStr, TelResult.class);
+                System.out.println(telResult.toString());
+            }finally {
+                response.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class TelResult{
+        private String mts;
+        private String province;
+        private String catName;
+        private String telString;
+        private String areaVid;
+        private String isVid;
+        private String carrier;
+
+        public String getMts() {
+            return mts;
+        }
+
+        public void setMts(String mts) {
+            this.mts = mts;
+        }
+
+        public String getProvince() {
+            return province;
+        }
+
+        public void setProvince(String province) {
+            this.province = province;
+        }
+
+        public String getCatName() {
+            return catName;
+        }
+
+        public void setCatName(String catName) {
+            this.catName = catName;
+        }
+
+        public String getTelString() {
+            return telString;
+        }
+
+        public void setTelString(String telString) {
+            this.telString = telString;
+        }
+
+        public String getAreaVid() {
+            return areaVid;
+        }
+
+        public void setAreaVid(String areaVid) {
+            this.areaVid = areaVid;
+        }
+
+        public String getIsVid() {
+            return isVid;
+        }
+
+        public void setIsVid(String isVid) {
+            this.isVid = isVid;
+        }
+
+        public String getCarrier() {
+            return carrier;
+        }
+
+        public void setCarrier(String carrier) {
+            this.carrier = carrier;
+        }
+
+        @Override
+        public String toString() {
+            return "TelResult{" +
+                    "mts='" + mts + '\'' +
+                    ", province='" + province + '\'' +
+                    ", catName='" + catName + '\'' +
+                    ", telString='" + telString + '\'' +
+                    ", areaVid='" + areaVid + '\'' +
+                    ", isVid='" + isVid + '\'' +
+                    ", carrier='" + carrier + '\'' +
+                    '}';
+        }
+    }
+}
