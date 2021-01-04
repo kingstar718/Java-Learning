b70cedee1d1bed5a6914d487f8b26dc02ab093c1
kingstar718 <kingstar718@foxmail.com>
2021-01-04 23:52:27 +0800

    循环

--- src/main/java/top/wujinxing/ch_2_basicjava/foreachloop/ForeachTest.java
+++ src/main/java/top/wujinxing/ch_2_basicjava/foreachloop/ForeachTest.java
@@ -11,37 +11,37 @@ import java.util.List;
  * @author: wujinxing
  * @date: 2019/3/14 09:13
  * @description: 不要在foreach循环里进行remove  或操作之后直接break结束循环体
- *
- *
  * foreach语法格式
  * for(元素类型t 元素变量x：遍历对象obj){
- *      引用了x的java语句
+ * 引用了x的java语句
  * }
  */
-public class Test1_foreach {
+public class ForeachTest {
 
-    public static void main(String[] args){
-        List<String> userNames = ImmutableList.of("Faker","Uzi","Clearlove","Thy Shy");
+    @Test
+    public void test1() {
+        List<String> userNames = ImmutableList.of("Faker", "Uzi", "Clearlove", "Thy Shy");
 
         System.out.println("使用for循环遍历List");
-        for (int i = 0; i < userNames.size(); i++){
+        for (int i = 0; i < userNames.size(); i++) {
             System.out.println(userNames.get(i));
         }
 
         //增强for循环，其实是依赖了while循环和Iterator实现的
         System.out.println("使用foreach循环遍历List");
-        for (String useName: userNames
-             ) {
+        for (String useName : userNames) {
             System.out.println(useName);
         }
 
+        userNames.forEach(u -> System.out.println(u));
 
+        userNames.forEach(System.out::println);
     }
 
     @Test
-    public void test(){
+    public void test() {
         //双括弧语法（double-brace syntax）建立并初始化一个List
-        List<String> userNames = new ArrayList<String>(){{
+        List<String> userNames = new ArrayList<String>() {{
             add("Faker");
             add("Uzi");
             add("Clearlove");
@@ -54,7 +54,7 @@ public class Test1_foreach {
             }
         }*/
 
-       //ConcurrentModificationException错误
+        //ConcurrentModificationException错误
         //ArrayList
         //final void checkForComodification() {
         //    if (modCount != expectedModCount)
@@ -69,8 +69,8 @@ public class Test1_foreach {
 
         Iterator iterator = userNames.iterator();
 
-        while (iterator.hasNext()){
-            if (iterator.next().equals("Faker")){
+        while (iterator.hasNext()) {
+            if (iterator.next().equals("Faker")) {
                 iterator.remove();
             }
         }
