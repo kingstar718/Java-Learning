2dd3681ab6791f91ea2bf92dc822d19c792673c7
kingstar718 <kingstar718@foxmail.com>
2021-09-11 23:59:54 +0800

    hbase

---
+++ src/main/java/top/wujinxing/ch_7_tools/hbase/HBaseTest.java
@@ -0,0 +1,15 @@
+package top.wujinxing.ch_7_tools.hbase;
+
+import org.junit.Test;
+
+/**
+ * @author wujinxing
+ * 2021-09-11
+ */
+public class HBaseTest {
+
+    @Test
+    public void test() {
+        
+    }
+}
