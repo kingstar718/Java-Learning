6a352dae3d3e39269787ddf82a0b3753ac1b8684
kingstar718 <kingstar718@foxmail.com>
2021-09-22 23:24:22 +0800

    log4j配置

--- src/main/java/top/wujinxing/ch_2_basicjava/array/ArrayCopy.java
+++ src/main/java/top/wujinxing/ch_2_basicjava/array/ArrayCopy.java
@@ -1,5 +1,7 @@
 package top.wujinxing.ch_2_basicjava.array;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 
 /**
@@ -7,22 +9,25 @@ import java.util.Arrays;
  * date 2019/9/24 20:52
  * description 数组的四种复制方式
  */
-public class array_copy {
+@Slf4j
+public class ArrayCopy {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] newArr =  Arrays.copyOf(arr, 4);
-        System.out.println(Arrays.toString(newArr)); //[1, 2, 3, 4]
+        int[] newArr = Arrays.copyOf(arr, 4);
+        //[1, 2, 3, 4]
+        log.info(Arrays.toString(newArr));
 
         int[] newArr2 = Arrays.copyOfRange(arr, 1, 4);
-        System.out.println(Arrays.toString(newArr2));//[2, 3, 4] 不包含终止索引
+        //[2, 3, 4] 不包含终止索引
+        log.info(Arrays.toString(newArr2));
 
-        int[] newArr3 = new  int[10];
+        int[] newArr3 = new int[10];
         //源数组，复制起始位置，目标数组，目标数组起始位置，复制长度
-        System.arraycopy(arr,1, newArr3,1, 5);
-        System.out.println(Arrays.toString(newArr3));
+        System.arraycopy(arr, 1, newArr3, 1, 5);
+        log.info(Arrays.toString(newArr3));
 
         int[] newArr4 = arr.clone();
-        System.out.println(Arrays.toString(newArr4));
+        log.info(Arrays.toString(newArr4));
     }
 }
