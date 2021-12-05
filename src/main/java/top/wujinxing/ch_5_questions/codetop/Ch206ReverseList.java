db0a6eda5df4b64bef063b8de88f9096f996e5ea
kingstar718 <kingstar718@foxmail.com>
2021-12-06 00:00:10 +0800

    code top reverse link list

---
+++ src/main/java/top/wujinxing/ch_5_questions/codetop/Ch206ReverseList.java
@@ -0,0 +1,27 @@
+package top.wujinxing.ch_5_questions.codetop;
+
+import org.junit.Test;
+
+/**
+ * @author wujinxing
+ */
+public class Ch206ReverseList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    @Test
+    public void test() {
+        ListNode l1 = new ListNode(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        System.out.println(l1);
+    }
+}
---
+++ src/main/java/top/wujinxing/ch_5_questions/codetop/ListNode.java
@@ -0,0 +1,51 @@
+package top.wujinxing.ch_5_questions.codetop;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PrimitiveIterator;
+
+/**
+ * @author wujinxing
+ * @date 2021-12-05
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    ListNode(int... values) {
+        List<ListNode> list = new ArrayList<>();
+        for (int i : values) {
+            list.add(new ListNode(i));
+        }
+        for (int i = 0; i < values.length - 1; i++) {
+            list.get(i).next = list.get(i + 1);
+        }
+        this.val = list.get(0).val;
+        this.next = list.get(0).next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out = new StringBuilder("[");
+        ListNode curr = this;
+        while (curr != null) {
+            out.append(curr.val).append(",");
+            curr = curr.next;
+        }
+        return out.substring(0, out.length() - 1) + "]";
+    }
+}
