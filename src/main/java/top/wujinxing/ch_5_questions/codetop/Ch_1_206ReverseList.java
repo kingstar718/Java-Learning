69c9df2a3eec9ce79fbab392fe563fb77214bd8a
kingstar718 <kingstar718@foxmail.com>
2021-12-06 23:59:18 +0800

    LRU

--- src/main/java/top/wujinxing/ch_5_questions/codetop/Ch_1_206ReverseList.java
+++ src/main/java/top/wujinxing/ch_5_questions/codetop/Ch_1_206ReverseList.java
@@ -1,19 +1,25 @@
 package top.wujinxing.ch_5_questions.codetop;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 /**
  * @author wujinxing
  */
-public class Ch206ReverseList {
+@Slf4j
+public class Ch_1_206ReverseList {
 
     public static ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
+            // 先存储下一个节点
             ListNode nextNode = curr.next;
+            // 将当前节点的下一个指向前一个
             curr.next = prev;
+            // 将前一个移到当前节点
             prev = curr;
+            // 将当前节点移到下一节点
             curr = nextNode;
         }
         return prev;
@@ -22,8 +28,8 @@ public class Ch206ReverseList {
     @Test
     public void test() {
         ListNode l1 = new ListNode(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        System.out.println(l1);
+        log.info("prev list: {}", l1);
         ListNode l2 = reverseList(l1);
-        System.out.println(l2);
+        log.info("reverse link list: {}", l2);
     }
 }
