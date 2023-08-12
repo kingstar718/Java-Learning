308a2c4bb2850960fb442250cbf18c75279257db
kingstar718 <kingstar718@foxmail.com>
2023-08-12 23:55:13 +0800

    es first

---
+++ tools/src/main/java/top/wujinxing/elasticsearch/EsUtils.java
@@ -0,0 +1,65 @@
+package top.wujinxing.elasticsearch;
+
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author kingstar718
+ * @date 2023-08-12
+ */
+@Slf4j
+public class EsUtils {
+
+    public static RestHighLevelClient buildHttpClient(String host, Integer port) {
+        return new RestHighLevelClient(RestClient.builder(new HttpHost(host, port, "http")));
+    }
+
+
+    public static void createIndex(RestHighLevelClient client, String indexName) throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest(indexName);
+        CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
+        log.info("create es index response: {}", response.isAcknowledged());
+    }
+
+
+    public static void existIndex(RestHighLevelClient client, String indexName) throws IOException {
+        GetIndexRequest request = new GetIndexRequest(indexName);
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        log.info("es index: {}, exists: {}", indexName, exists);
+    }
+
+    public static void deleteIndex(RestHighLevelClient client, String indexName) throws IOException {
+        DeleteIndexRequest request = new DeleteIndexRequest(indexName);
+        AcknowledgedResponse response = client.indices().delete(request, RequestOptions.DEFAULT);
+        log.info("es index: {}, delete: {}", indexName, response.isAcknowledged());
+    }
+
+
+    public static void addDoc(RestHighLevelClient client, String indexName, Object o) throws IOException {
+        IndexRequest request = new IndexRequest(indexName);
+        request.id(UUID.randomUUID().toString().replace("-", ""));
+        request.timeout(TimeValue.timeValueSeconds(3));
+        request.source(JSON.toJSONString(o), XContentType.JSON);
+        IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+        log.info("add doc response status: {}", response.status());
+        log.info("add doc response: {}", response);
+    }
+
+}
---
+++ tools/src/test/java/top/wujinxing/EsTest.java
@@ -0,0 +1,40 @@
+package top.wujinxing;
+
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
+import top.wujinxing.elasticsearch.EsUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author kingstar718
+ * @date 2023-08-12
+ */
+public class EsTest {
+
+    @Test
+    public void indexTest() throws IOException {
+        RestHighLevelClient client = EsUtils.buildHttpClient("127.0.0.1", 9200);
+        String indexName = "test_index";
+        EsUtils.createIndex(client, indexName);
+        EsUtils.existIndex(client, indexName);
+        EsUtils.deleteIndex(client, indexName);
+    }
+
+
+    @Test
+    public void addDocTest() throws IOException {
+        RestHighLevelClient client = EsUtils.buildHttpClient("127.0.0.1", 9200);
+        String indexName = "test_index";
+        EsUtils.createIndex(client, indexName);
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", System.currentTimeMillis());
+        map.put("name", "李白");
+        map.put("age", 18);
+        EsUtils.addDoc(client, indexName, map);
+    }
+}
